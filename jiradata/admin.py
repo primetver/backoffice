dd7467d8520b47494c3a7c94223a7c172ec98afb
Nikolay Ishiev <ishiev@primetver.ru>
2018-12-13 18:14:18 +0300

    Add workload report from jira data

--- jiradata/admin.py
+++ jiradata/admin.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
+from datetime import date, timedelta
 
-from .models import (JiraIssue, Worklog, Customfield)
+from django.contrib import admin
+from django.utils import timezone
+from monthdelta import monthdelta, monthmod
+
+from .models import Customfield, JiraIssue, JiraUser, Worklog, WorklogReport
+
+COLUMNS = 18
 
 # Register your models here.
 
@@ -39,3 +45,120 @@ class CustomfieldAdmin(JiraAdmin):
     Определения пользовательских полей
     '''
     list_display = ('cfname', 'description')
+
+
+
+@admin.register(WorklogReport)
+class WorklogReportAdmin(JiraAdmin):
+    '''
+    Отчет о месячной фактической загрузке сотрудника в проектах
+    '''
+    change_list_template = 'admin/workload_user_change_list.html'
+
+    class ReportListFilter(admin.SimpleListFilter):
+        # Замена функции выбора, так чтобы значение по умолчанию вместо "Все" было "Текущий"
+        def choices(self, changelist):
+            yield {
+                'selected': self.value() is None,
+                'query_string': changelist.get_query_string(remove=[self.parameter_name]),
+                'display': 'Текущий',
+            }
+            for lookup, title in self.lookup_choices:
+                yield {
+                    'selected': self.value() == str(lookup),
+                    'query_string': changelist.get_query_string({self.parameter_name: lookup}),
+                    'display': title,
+                }
+
+    # Фильтр отображения по пользователям Jira 
+    class UserFilter(ReportListFilter):
+        # Human-readable title which will be displayed in the
+        # right admin sidebar just above the filter options.
+        title = 'Сотрудники'
+
+        # Parameter for the filter that will be used in the URL query.
+        parameter_name = 'user'
+
+        # формирование перечня сотрудников в зависимости от прав
+        def lookups(self, request, model_admin):
+            if request.user.has_perm('jiradata.view_all'):
+                users = JiraUser.objects.filter(active=1)
+                return ( (user.user_name, str(user)) for user in users )
+            return ()
+            
+        def queryset(self, request, queryset):
+             # не фильтруем, фильтр вычисляется в changelist_view()
+            return queryset
+    
+    # Фильтр отображения по годам
+    class YearFilter(ReportListFilter):
+        # Human-readable title which will be displayed in the
+        # right admin sidebar just above the filter options.
+        title = 'Данные за год'
+
+        # Parameter for the filter that will be used in the URL query.
+        parameter_name = 'year'
+
+        def lookups(self, request, model_admin):
+            qs = model_admin.get_queryset(request).order_by('startdate')
+            
+            # ограничение диапазона данными по себе, если нет прав смотреть по всем
+            if not request.user.has_perm('jiradata.view_all'):
+                qs = qs.filter(author=request.user.username)
+
+            try:
+                first_year = qs.first().startdate.year
+                last_year = qs.last().startdate.year
+                return ( (year, year) for year in range(first_year, last_year + 1) )
+            except (AttributeError):
+                return ()
+
+        def queryset(self, request, queryset):
+            # не фильтруем, фильтр вычисляется в changelist_view()    
+            return queryset
+    
+
+    list_display_links = None
+    list_filter = (
+        YearFilter,
+        UserFilter
+    )
+    
+    # Отображение списка бюджетов и месячной загруженности по выбранному сотруднику
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context
+        )
+
+        # pylint: disable=no-member
+        request.user.username
+        
+        try:
+            # извлечение данных запроса
+            cl = response.context_data['cl']
+            qs = cl.queryset
+            year = cl.get_filters_params().get("year", None)
+            # пользователь из параметров фильтра или текущий
+            user = cl.get_filters_params().get('user', request.user.username)
+        
+            # проверка прав на просмотр указанного в запросе сотрудникa
+            if not request.user.has_perm('jiradata.view_all') and user != request.user.username:
+                return response
+            
+            # проверка параметра year
+            if year:
+                month_from = date(int(year), 1, 1)
+            else:
+                now = timezone.now()
+                month_from = date(now.year, now.month, 1) - monthdelta(COLUMNS-2)
+
+            month_list = [month_from + monthdelta(i) for i in range(COLUMNS)]
+        except (AttributeError, KeyError, ValueError):
+            return response
+
+        response.context_data['months'] = month_list
+        response.context_data['member'] = JiraUser.objects.filter(user_name=user).first()
+        response.context_data['summary'], response.context_data['total'] = qs.get_workload(month_list, user)
+        
+        return response
--- jiradata/models.py
+++ jiradata/models.py
@@ -1,4 +1,10 @@
+# pylint: disable=no-member
+from datetime import date, timedelta
+
+import pandas as pd
 from django.db import models as md
+from django_pandas.io import read_frame
+from monthdelta import monthdelta, monthmod
 
 
 class JiraModel(md.Model):
@@ -26,9 +32,12 @@ class BudgetCustomField:
     # TODO: параметр настройки
     budget_customfield = '10700'
 
+    def __init__(self, id=None):
+        self._issueid = id
+
     def get_issueid(self):
         # должно быть переопределено
-        return None
+        return self._issueid
 
     def customfield_value(self, customfield):
         cfv = CustomfieldValue.objects.filter(issue=self.get_issueid(), customfield=customfield).get()
@@ -43,6 +52,31 @@ class BudgetCustomField:
         return CustomfieldOption.objects.filter(id=id).get().customvalue
     budget_name.short_description = 'Бюджет проекта'
 
+#
+#  Модели JIRA
+#
+
+class JiraUser(JiraModel):
+    '''
+    Пользователь JIRA
+    '''
+    class Meta(JiraModel.Meta):
+        db_table = 'cwd_user'
+        verbose_name = 'пользователь Jira'
+        verbose_name_plural = 'пользователи Jira'
+
+    id = md.DecimalField('ID', primary_key=True, max_digits=18, decimal_places=0)
+    directory_id = md.DecimalField('ID', max_digits=18, decimal_places=0)
+    user_name = md.CharField('Пользователь', max_length=255, db_column='lower_user_name')
+    active = md.DecimalField('Проект', max_digits=9, decimal_places=0)
+    created_date = md.DateTimeField('Дата создания')
+    updated_date = md.DateTimeField('Дата изменения')
+    first_name = md.CharField('Фамилия', max_length=255)
+    last_name = md.CharField('Имя', max_length=255)
+    email_address = md.CharField('E-Mail', max_length=255, db_column='lower_email_address')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class JiraIssue(JiraModel, BudgetCustomField):
@@ -185,4 +219,86 @@ class CustomfieldOption(JiraModel):
     def __str__(self):
         return self.customvalue
 
-    
+
+#
+# Отчеты
+#
+
+class WorklogReport(Worklog):
+    ''' 
+    Прокси-модель для агрегированного отчета о фактической загрузке
+    '''
+    class Meta():
+        proxy = True
+        verbose_name = 'фактическая загрузка по сотруднику'
+        verbose_name_plural = 'отчет о фактической загрузке по сотруднику'
+        permissions = (
+            ("view_all", "Просмотр любого сотрудника"),
+        )
+
+    class Manager(md.Manager):
+        def get_queryset(self):
+            return WorklogReport.QuerySet(self.model, using='jira')
+
+    # замена менеджера по умолчанию
+    objects = Manager()
+
+    class QuerySet(md.QuerySet):
+        '''
+        Дополненный класс запроса для вывода агрегированных затрат времени по пользователю
+        '''
+        # Формирование набора данных  
+        def get_workload(self, month_list, user):
+             # фильтрация запроса по времени и указанному пользователю
+            qs = self.filter(
+                startdate__range=(month_list[0], month_list[-1]), 
+                author=user
+            )
+
+            # чтение журнала работ во фрейм
+            df = read_frame(
+                qs,
+                fieldnames=[
+                    'startdate',
+                    'timeworked',            
+                ],
+                index_col = 'issueid'
+            )
+            if df.empty: return [], None
+
+            # извлекаем уникальный массив задач
+            issues = df.index.unique()
+            # создаем фрейм названий бюджетов для задач
+            bdf = pd.DataFrame(
+                ( BudgetCustomField(issueid).budget_name() for issueid in issues ),
+                index=issues,
+                columns=['budget'])
+
+            # объединение в один
+            wdf = pd.merge(df, bdf, left_index=True, right_index=True)
+            wdf['month'] = wdf['startdate'].map(lambda x: date(year=x.year, month=x.month, day=1))
+            wdf['hours'] =  wdf['timeworked'] / 3600
+
+            # агрегирование по бюджетам и месяцам, получаем фрейм с иерархическим индексом: проект, месяц
+            month_worklog = wdf.groupby(['budget', 'month']).sum()
+
+            # результат -- кортеж:
+            # 1) генератор последовательности словарей с полями: 
+            #   - бюджет, 
+            #   - список помесячных записей о загрузке для каждого месяца из переданного списка
+            #     (отсутствующие данные заполняются нулями)
+            # 2) итоговая строка 
+            return (
+                (
+                    {
+                        'project': p,
+                        # сбрасываем индекс по проектам, переиндексируем по списку месяцев, выводим в список словарей
+                        'workload': worklog.reset_index(level=0, drop=True).reindex(month_list, fill_value=0).to_dict('records')
+                    } for p, worklog in month_worklog.groupby(level='budget')
+                ),
+                {
+                    # дополнительная итоговая строка 
+                    'workload': month_worklog.groupby('month').sum().reindex(month_list, fill_value=0).to_dict('records')
+                }
+            )    
+
