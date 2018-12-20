39306ca73517391f043cec911cc1c38d1a2437ee
Nikolay Ishiev <ishiev@primetver.ru>
2018-12-20 18:54:53 +0300

    Workdays models,  admin, work time report

--- workdays/admin.py
+++ workdays/admin.py
@@ -1,5 +1,12 @@
-from django.contrib import admin
-from .models import HolidayName, SpecialDay
+# pylint: disable=no-member
+from datetime import date, timedelta
+
+from django.contrib import admin, messages
+from django.db import transaction
+from django.utils import timezone
+
+from .models import HolidayName, SpecialDay, WorktimeStandards
+from .utils import standards_report
 
 # Register your models here.
 
@@ -7,9 +14,32 @@ from .models import HolidayName, SpecialDay
 @admin.register(HolidayName)
 class HolidayNameAdmin(admin.ModelAdmin):
     '''
-    Справочник нерабочих дней
+    Справочник праздников
     '''
-    list_display = ('dayname',)
+    @transaction.atomic
+    def make_published_year(self, request, queryset, year):
+        for hd in queryset:
+            for n in range(hd.count):
+                SpecialDay.objects.update_or_create(
+                    date=date(year, hd.month, hd.day) + timedelta(n),
+                    defaults={
+                        'daytype':SpecialDay.HOLIDAY,
+                        'dayname':hd
+                    }
+                )
+        self.message_user(request, f'Праздники добавлены в календарь {year} года')
+    
+    def make_published(self, request, queryset):
+        return self.make_published_year(request, queryset, timezone.now().year)
+    make_published.short_description = 'Добавить в календарь текущего года'
+
+    def make_published_next(self, request, queryset):
+        return self.make_published_year(request, queryset, timezone.now().year + 1)
+    make_published_next.short_description = 'Добавить в календарь следующего года'
+
+    list_display = ('dayname','month', 'day', 'count')
+    actions = (make_published, make_published_next)
+
 
 
 @admin.register(SpecialDay)
@@ -17,4 +47,77 @@ class SpecialDayAdmin(admin.ModelAdmin):
     '''
     Редактирование календаря особых дней
     '''
-    list_display = ('date', 'daytype', 'dayname', 'shorthours')
+    list_display = ('date', 'daytype', 'dayname')
+    list_filter = ('date', 'daytype', 'dayname') 
+    date_hierarchy = 'date'   
+
+
+@admin.register(WorktimeStandards)
+class WorktimeStandardsAdmin(admin.ModelAdmin):
+    '''
+    Отчет по нормам рабочего времени 
+    '''
+    change_list_template = 'admin/worktime_standards_change_list.html'
+
+    # Фильтр отображения по годам
+    class YearFilter(admin.SimpleListFilter):
+        # Human-readable title which will be displayed in the
+        # right admin sidebar just above the filter options.
+        title = 'Данные за год'
+
+        # Parameter for the filter that will be used in the URL query.
+        parameter_name = 'year'
+
+        # Замена функции выбора, так чтобы значение по умолчанию вместо "Все" было "Текущий"
+        def choices(self, changelist):
+            yield {
+                'selected': self.value() is None,
+                'query_string': changelist.get_query_string(remove=[self.parameter_name]),
+                'display': 'Текущий',
+            }
+            for lookup, title in self.lookup_choices:
+                yield {
+                    'selected': self.value() == str(lookup),
+                    'query_string': changelist.get_query_string({self.parameter_name: lookup}),
+                    'display': title,
+                }
+        # Перечень значений фильтра для выбора
+        def lookups(self, request, model_admin):
+            qs = model_admin.get_queryset(request).order_by('date')
+
+            first_year = qs.first().date.year
+            last_year = qs.last().date.year
+            
+            return ( (year, year) for year in range(first_year, last_year + 1) )
+
+        def queryset(self, request, queryset):
+            # не фильтруем, фильтр по датам вычисляется в changelist_view()    
+            return queryset
+
+    list_display_links = None
+    list_filter = (YearFilter,)
+    
+    # Данные можно только смотреть
+    def has_add_permission(self, request, extra_context=None): return False
+    def has_change_permission(self, request, extra_context=None): return False
+    def has_delete_permission(self, request, extra_context=None): return False
+
+    # Отображение отчета по нормам рабочего времени
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context
+        )
+
+        try:
+            cl = response.context_data['cl']
+            qs = cl.queryset
+            year = int(cl.get_filters_params().get("year", timezone.now().year))
+        except (AttributeError, KeyError, TypeError, ValueError):
+            return response
+
+        response.context_data['year'] = year
+        response.context_data['header'] = ['Дней', 'Рабочих дней', 'Выходных', 'Рабочих часов']
+        response.context_data['summary'] = standards_report(year)
+
+        return response
--- workdays/models.py
+++ workdays/models.py
@@ -1,9 +1,10 @@
 # pylint: disable=no-member
+from datetime import date
+
+from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models as md
-
-# TODO: настройка
-WORKHOURS = 8
+from django.utils import timezone
 
 # Create your models here.
 
@@ -14,8 +15,18 @@ class HolidayName(md.Model):
     class Meta():
         verbose_name = 'наименование праздника'
         verbose_name_plural = 'наименования праздников'
+        ordering = ('month', 'day')
 
-    dayname = md.CharField('Название', max_length=200)
+    dayname = md.CharField('Название', max_length=150)
+    month = md.IntegerField('Месяц', default=1, validators=[MinValueValidator(1), MaxValueValidator(12)])
+    day = md.IntegerField('День', default=1, validators=[MinValueValidator(1), MaxValueValidator(31)])
+    count = md.IntegerField('Число дней', default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
+
+    def clean(self):
+        try:
+            date(timezone.now().year, self.month, self.day)
+        except ValueError:
+            raise ValidationError('Введите корректное календарное значение месяца и дня праздника')
     
     def __str__(self):
         return self.dayname
@@ -26,31 +37,42 @@ class SpecialDay(md.Model):
     Календарь праздников, сокращенных и рабочих выходных дней
     '''
     class Meta():
-        verbose_name = 'особый календарный день'
-        verbose_name_plural = 'особые календарные дни'
+        verbose_name = 'день производственного календаря'
+        verbose_name_plural = 'дни производственного календаря'
+        ordering = ('date',)
 
 
     HOLIDAY = 'HL'
     SHORTENED = 'SH'
     WORK = 'WK'
     DAYTYPE_CHOICES = (
-        (HOLIDAY,   'Нерабочий день'),
+        (HOLIDAY,   'Выходной день'),
         (SHORTENED, 'Сокращенный день'),
         (WORK, 'Рабочий день')
     )
 
-    date = md.DateField('Дата', db_index=True)
-    dayname = md.ForeignKey(HolidayName, null=True, on_delete=md.PROTECT, verbose_name='Название')
+    date = md.DateField('Дата', db_index=True, unique=True)
     daytype = md.CharField('Тип дня', max_length=2, default=HOLIDAY, choices=DAYTYPE_CHOICES)
-    shorthours = md.IntegerField('Сокращение дня, час', default=0,
-        validators=[MinValueValidator(0), MaxValueValidator(WORKHOURS)])
+    dayname = md.ForeignKey(HolidayName, null=True, blank=True, on_delete=md.PROTECT, verbose_name='Название')
 
-    def clean(self):
-        if self.daytype != SpecialDay.SHORTENED:
-            if self.daytype != SpecialDay.HOLIDAY:
-                self.shorthours = WORKHOURS
-            else:
-                self.shorthours = 0
-
+    def workdayholidayhours(self, hours):
+        if self.daytype == SpecialDay.HOLIDAY:
+            return (0, 1, 0)
+        elif self.daytype == SpecialDay.SHORTENED:
+            return (1, 0, hours - 1)
+        else:
+            return (1, 0, hours)
+    
     def __str__(self):
         return f'{self.get_daytype_display()} {self.date:%d.%m.%Y} {self.dayname or ""}'.rstrip()
+
+
+class WorktimeStandards(SpecialDay):
+    '''
+    Модель отчета по нормам рабочего времени
+    '''
+    class Meta():
+        proxy = True
+        verbose_name = 'норма рабочего времени'
+        verbose_name_plural = 'нормы рабочего времени'
+        default_permissions = ('view',)
